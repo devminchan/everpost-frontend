bdd5e985b5968357673e30446f44c2e287985b0e
devminchan <minchan.dev@gmail.com>
2020-03-24 15:38:40 +0900

    PostCard 추가

---
+++ src/components/PostCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <div>{{ post.content }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+export interface PostCardData {
+  id: number;
+  title: string;
+  content: string;
+  fileUrls: string[];
+}
+
+@Component
+export default class PostCard extends Vue {
+  @Prop(Object)
+  post!: PostCardData;
+}
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,46 @@
 <template>
-  <div>A</div>
+  <ul>
+    <li v-for="post in posts" :key="post.id">
+      <PostCard :post="post"></PostCard>
+    </li>
+  </ul>
 </template>
 
-<script>
-// @ is an alias to /src
-export default {
-  name: "Home"
-};
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import PostCard, { PostCardData } from '@/components/PostCard.vue';
+
+@Component({
+  components: {
+    PostCard,
+  },
+})
+export default class Home extends Vue {
+  data() {
+    const datas: PostCardData[] = [
+      {
+        id: 1,
+        title: 'Title1',
+        content: 'Content',
+        fileUrls: [],
+      },
+      {
+        id: 2,
+        title: 'Title2',
+        content: 'Content',
+        fileUrls: [],
+      },
+      {
+        id: 3,
+        title: 'Title3',
+        content: 'Content',
+        fileUrls: [],
+      },
+    ];
+
+    return {
+      posts: datas,
+    };
+  }
+}
 </script>
