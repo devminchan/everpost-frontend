845c4094cc890e76f74cdd1ca3ccbd0f2bbaa142
devminchan <minchan.dev@gmail.com>
2020-03-26 09:42:10 +0900

    Login view 스타일링

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,18 @@
 <template>
-  <div>
-    <input type="email" v-model="email" />
-    <br />
-    <input type="password" v-model="password" />
-    <br />
-    <button @click="handleLogin">Login</button>
+  <div id="login-form-container">
+    <div id="login-form">
+      <span>Email</span>
+      <br />
+      <input type="email" v-model="email" />
+      <br />
+      <br />
+      <span>Password</span>
+      <br />
+      <input type="password" v-model="password" />
+      <br />
+      <br />
+      <button @click="handleLogin">Login</button>
+    </div>
   </div>
 </template>
 
@@ -22,3 +30,35 @@ export default class Login extends Vue {
   }
 }
 </script>
+
+<style lang="scss">
+#login-form {
+  background: white;
+  width: 40%;
+  @media (max-width: 768px) {
+    width: 320px;
+  }
+
+  span {
+    float: left;
+  }
+
+  input {
+    width: 100%;
+    font-size: 20px;
+  }
+
+  button {
+    width: 30%;
+    font-size: 24px;
+    background-color: white;
+    border: 1px solid rgba($color: #2c3e50, $alpha: 0.2);
+  }
+}
+
+#login-form-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
