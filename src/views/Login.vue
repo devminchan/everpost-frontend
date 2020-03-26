96797a8e2ddea81989f413e0fe78af87eaf21a1a
devminchan <minchan.dev@gmail.com>
2020-03-26 09:24:25 +0900

    Login 폼 화면 페이지 추가

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="email" v-model="email" />
+    <br />
+    <input type="password" v-model="password" />
+    <br />
+    <button @click="handleLogin">Login</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+  email = '';
+  password = '';
+
+  handleLogin() {
+    console.log(this.email, this.password);
+  }
+}
+</script>
